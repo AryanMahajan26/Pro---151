ac65cb1c63cfa606230033c862765b344ff0b068
IntenseAqua <99726711+IntenseAqua@users.noreply.github.com>
2022-12-06 19:49:55 +0530

    Add files via upload

---
+++ Car.js
@@ -0,0 +1,16 @@
+AFRAME.registerComponent("car-model", {
+    schema: {
+        modelRef: {type: "string", default: "assets/scene.gltf"},
+        clickCounter: {type: "number", default: 0},
+    },
+
+    init: function(){
+        //Do something when component first attached
+        this.el.setAttribute("gltf-model", this.data.modelRef);
+        const position = {x: -10, y: 5, z: 20};
+        const rotation = {x: 0, y: -10, z: 0};
+        this.el.setAttribute("position", position);
+        this.el.setAttribute("rotation", rotation);
+        }
+
+});
